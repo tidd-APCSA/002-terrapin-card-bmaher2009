97dbe3153c9fc9394c904ce7d6d3812885d525c6
Brendan Maher <brendangmaher@gmail.com>
2020-10-16 20:11:41 +0000

    final version

--- src/main/java/TerrapinCard.java
+++ src/main/java/TerrapinCard.java
@@ -1,4 +1,56 @@
 public class TerrapinCard {
+  private double balance;
+
+  public TerrapinCard(double theBalance){
+    balance = theBalance;
+  }
+  
+  public void payEconomical() {
+    if (balance>=2.50){
+      balance-=2.50;
+    }
+    else if (balance<2.50){
+      balance = balance; 
+    }
+
+}
+
+public void payGourmet() {
+    if (balance>=4){
+      balance-=4;
+    }
+    else if (balance<4){
+      balance = balance;
+    }
+}
+
+public void loadMoney(double amount) {
+  if(balance+amount < 0){
+    balance = balance;
+  }
+  else if(balance+amount >= 150){
+    balance = 150;
+  }
+  else if(balance+amount < 150){
+    balance = balance+amount;
+  }
+  
+}
+
+
+
+
+
+
+
+
+
+
+  public String toString(){
+    return "The card has " + balance +" dollars";
+  }
+
+
 
 
 }
